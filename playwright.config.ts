0bede62a35f9b9c0a0234b5e1d22503b169070a4
Duriel A. Timatim <durieltims@gmail.com>
2025-04-10 15:31:53 +0800

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [['junit', { outputFile: 'results.xml' }]],
+  reporter: [['junit', { outputFile: 'test-results/results.xml' }]],
   // use: {
   //   browserName: 'chromium',
   //   channel: 'chrome', // Use Google Chrome
