d861a6a36431a39773754f928dd9c292568c5bf1
Duriel A. Timatim <durieltims@gmail.com>
2025-04-30 10:05:40 +0800

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers:4,
+  // workers: process.env.CI ? 1 : undefined,
   reporter: [['junit', { outputFile: 'test-results/results.xml' }]],
   // use: {
   //   browserName: 'chromium',
